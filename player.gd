5e03c6962db412c55f920368f69c64c6a67ccf11
chaggle <chaggle@foxmail.com>
2026-09-07 21:59:11 +0800

    fix: fix bugs and add animation play

--- player.gd
+++ player.gd
@@ -17,9 +17,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 		
 		
 	if velocity.length() > 0:
@@ -29,3 +29,13 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play()
 	position += velocity * 	delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif  velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
